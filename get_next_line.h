1ca6e7300b6fa842ee2a65e7e00ca199c8122722
Linh Nguyen <linhnguy@c3r5p5.hive.fi>
2023-12-15 14:50:51 +0200

    header and start two .C

---
+++ get_next_line.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: linhnguy <linhnguy@hive.student.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/15 14:16:53 by linhnguy          #+#    #+#             */
+/*   Updated: 2023/12/15 14:45:24 by linhnguy         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# include <unistd.h>
+# include <stdlib.h>
+
+typedef struct s_list
+{
+	char			str;
+	struct s_list	*next;
+}	t_list;
+
+#endif
