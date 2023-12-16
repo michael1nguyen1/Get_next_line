591c1da0a96206818b1bbc3f9c0b556f3915779c
Linh Nguyen <linhnguy@c1r2p4.hive.fi>
2023-12-16 20:42:24 +0200

    blueprint and funciton to make list

--- get_next_line.c
+++ get_next_line.c
@@ -6,11 +6,62 @@
 /*   By: linhnguy <linhnguy@hive.student.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/15 14:12:00 by linhnguy          #+#    #+#             */
-/*   Updated: 2023/12/15 14:28:14 by linhnguy         ###   ########.fr       */
+/*   Updated: 2023/12/16 20:40:58 by linhnguy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "get_next_line.h"
+
+
+int	find_line(t_list *list)
+{
+	int	i;
+
+	if (list == NULL)
+		return (0);
+	while (list)
+	{
+		i = 0;
+		while (list->str[i] && i < BUFFER_SIZE)
+		{
+			if (list->str[i] == '\n')
+				return (1);
+			i++;
+		}
+		list = list->next;
+	}
+	return (0);
+}
+
+void	make_list(t_list **list, int fd)
+{
+	int		buff_read;
+	char	*buf;
+
+	while (!find_line(*list))
+	{
+		buf = malloc(BUFFER_SIZE + 1);
+		if (!buf)
+			return ;
+		buff_read = read(fd, buf, BUFFER_SIZE);
+		if (!buff_read)
+			return ;
+		buf[buff_read] = '\0';
+		addnode(list, buf);
+	}
+}
+
 char	*get_next_line(int fd)
 {
-	
+	static t_list	*list = NULL;
+	char			*nextline;
+
+	if (fd < 0 || BUFFER_SIZE <= 0 || read (fd, &nextline, 0) < 0)
+		return (NULL);
+	make_list(&list, fd);
+	if (list == NULL)
+		return (NULL);
+	nextline = get_nextl(list);
+	clean_list_(&list);
+	return (nextline);
 }
--- get_next_line.h
+++ get_next_line.h
@@ -6,18 +6,23 @@
 /*   By: linhnguy <linhnguy@hive.student.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/15 14:16:53 by linhnguy          #+#    #+#             */
-/*   Updated: 2023/12/15 14:45:24 by linhnguy         ###   ########.fr       */
+/*   Updated: 2023/12/16 20:41:19 by linhnguy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 10
+# endif
+
 # include <unistd.h>
 # include <stdlib.h>
 
 typedef struct s_list
 {
-	char			str;
+	char			*str;
 	struct s_list	*next;
 }	t_list;
 
